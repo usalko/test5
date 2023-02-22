7db29dcbd8b2ac9d7dd47ba931dc59342512eeda
Vanya U <ivict@rambler.ru>
2023-02-22 07:43:28 +0300

    # There is the responsive layout for charts.

--- src/components/dashboards/ReactionsBoard.tsx
+++ src/components/dashboards/ReactionsBoard.tsx
@@ -1,3 +1,4 @@
+import { Box, Flex, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
 
 
@@ -9,6 +10,21 @@ export const ReactionsBoard: React.FC<ReactionsBoardProps> = ({ className = '' }
 
     return (
         <div className={className}>
+            <VStack>
+                <Flex w='100%' flexDirection={{ base: 'column', md: 'row', lg: 'row', xl: 'row', '2xl': 'row' }}>
+                    <Box flex='1' alignContent='left'>
+                        <Text>Chart 1</Text>
+                    </Box>
+                    <Box flex='1' alignContent='left'>
+                        <Text>Chart 2</Text>
+                    </Box>
+                </Flex>
+                <Flex w='100%'>
+                    <Box flex='1' alignContent='left'>
+                        <Text>Table</Text>
+                    </Box>
+                </Flex>
+            </VStack>
         </div>
     )
 }
