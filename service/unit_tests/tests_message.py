99f7aa5b56c2b259a48381dd0864cedbd16bf0ed
Vanya U <ivict@rambler.ru>
2023-03-01 00:56:31 +0300

    # There is the fix for data generation.

--- service/unit_tests/tests_message.py
+++ service/unit_tests/tests_message.py
@@ -1,10 +1,11 @@
 from datetime import date, datetime, timedelta, timezone
-from random import randint
+from random import randint, sample
 
 from emojis.models import TgChat, TgInputPeer, TgMessage, TgReaction, TgUser, TgMessageViews
 from faker import Faker
 from unit_tests.snapshot_database_test_case import \
     SnapshotDatabaseStateTestCase
+from itertools import chain, combinations
 
 
 class TestsMessage(SnapshotDatabaseStateTestCase):
@@ -35,7 +36,8 @@ class TestsMessage(SnapshotDatabaseStateTestCase):
         TgChat.objects.create(type=TgChat.ChatType.PRIVATE,
                               title='simple private chat')
 
-        emoticons = ['\U0001F600', '\U0001F603', '\U0001F604', '\U0001F601', '\U0001F606']
+        emoticons = ['\U0001F600', '\U0001F603',
+                     '\U0001F604', '\U0001F601', '\U0001F606']
 
         for i in range(0, 100):
             message = TgMessage.objects.create(stamp=datetime.utcnow(),
@@ -44,14 +46,20 @@ class TestsMessage(SnapshotDatabaseStateTestCase):
                 pk=randint(1, 3)),
                 chat=TgChat.objects.get(pk=1)
             )
-            TgReaction.objects.create(stamp=datetime.utcnow(),
-                                      peer=TgInputPeer.objects.get(
-                                          pk=randint(1, 3)),
-                                      message=message,
-                                      type=TgReaction.ReactionType.EMOJI,
-                                      emoticon=emoticons[randint(
-                                          0, len(emoticons) - 1)]
-                                      )
+
+            possible_reacting_users = [e for e in list(
+                chain(*map(lambda x: combinations([1, 2, 3], x), range(0, 3+1)))) if len(e) > 1]
+
+            for samples in sample(possible_reacting_users, 1):
+                for pk in samples:
+                    TgReaction.objects.create(stamp=datetime.utcnow(),
+                                              peer=TgInputPeer.objects.get(
+                                                  pk=pk),
+                                              message=message,
+                                              type=TgReaction.ReactionType.EMOJI,
+                                              emoticon=emoticons[randint(
+                                                  0, len(emoticons) - 1)]
+                                              )
             for peer_pk in range(1, 4):
                 TgMessageViews.objects.create(stamp=datetime.utcnow(),
                                               peer=TgInputPeer.objects.get(
